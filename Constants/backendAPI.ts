e22a01755733690b537e90fccbe6a5ff68170320
iori0213 <iori19980213@gmail.com>
2022-08-23 12:48:30 +0800

    update api const

--- Constants/backendAPI.ts
+++ Constants/backendAPI.ts
@@ -1,5 +1,7 @@
 export const BackendUrl = `http://localhost:5000`;
 export const AuthAPI = `${BackendUrl}/api/v1/auth`;
+export const TagAPI = `${BackendUrl}/api/v1/tag`;
+
 export const UserAPI = `${BackendUrl}/api/v1/user`;
 export const FriendAPI = `${BackendUrl}/api/v1/friendship`;
 export const ChatRoomAPI = `${BackendUrl}/api/v1/chatroom`;
