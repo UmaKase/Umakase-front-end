7cf677c1cf635cd896f7d49a82a17133b72c5819
iori0213 <iori19980213@gmail.com>
2022-08-26 15:09:59 +0800

    SearchBar_V1 component for Initial config screen

---
+++ Components/InitialStep/SearchBar.tsx
@@ -0,0 +1,78 @@
+import {
+  Alert,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import {
+  windowWidth,
+  windowHeight,
+  drawerColor,
+} from "../../Constants/cssConst";
+import { FontAwesome } from "@expo/vector-icons";
+
+type Props = {
+  placeholderText: string;
+  input: string;
+  setInput: (value: React.SetStateAction<string>) => void;
+  textOnChange: () => void;
+  searchBtnFunc: () => void;
+};
+
+const SearchBar: React.FC<Props> = ({
+  input,
+  setInput,
+  placeholderText,
+  textOnChange,
+  searchBtnFunc,
+}) => {
+  return (
+    <View style={styles.searchbarContainer}>
+      <TextInput
+        style={styles.searchbar}
+        onChangeText={(newText) => setInput(newText)}
+        value={input}
+        placeholder={placeholderText}
+        placeholderTextColor={drawerColor}
+        autoCapitalize="none"
+        // caretHidden={true}
+        selectionColor="#FFF"
+      ></TextInput>
+      <View style={styles.searchBtn}>
+        <TouchableOpacity
+          onPress={() =>
+            Alert.alert("this is search btutton", "Hi! I am search button.")
+          }
+        >
+          <FontAwesome name="search" size={windowWidth * 0.07} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default SearchBar;
+
+const styles = StyleSheet.create({
+  searchbarContainer: {
+    width: windowWidth,
+    height: windowHeight * 0.06,
+    flexDirection: "row",
+    paddingLeft: windowWidth * 0.1,
+    paddingRight: windowWidth * 0.05,
+  },
+  searchbar: {
+    flex: 5,
+    fontSize: windowWidth * 0.065,
+    color: "#FFF",
+    borderBottomWidth: 1,
+    borderColor: "#FFF",
+  },
+  searchBtn: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
