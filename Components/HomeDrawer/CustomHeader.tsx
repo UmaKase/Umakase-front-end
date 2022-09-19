8f7aa4147f9328ccaec0d8ccdc8727c63de3179f
iori0213 <iori19980213@gmail.com>
2022-09-19 21:27:18 +0800

    update component and random screen

---
+++ Components/HomeDrawer/CustomHeader.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Alert, StyleSheet, View } from "react-native";
+import { lightTextColor, windowWidth } from "../../Constants/cssConst";
+import { FontAwesome } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+interface CustomHeaderProps {
+  toggleMenu: () => void;
+}
+
+const CustomHeader: React.FC<CustomHeaderProps> = ({ toggleMenu }) => {
+  return (
+    <View style={styles.container}>
+      <View style={{ flex: 1 }}>
+        <TouchableOpacity onPress={() => toggleMenu()}>
+          <FontAwesome name="bars" size={30} color={lightTextColor} />
+        </TouchableOpacity>
+      </View>
+      <View style={{ flex: 1, alignItems: "flex-end" }}>
+        <TouchableOpacity
+          style={styles.iconContainer}
+          onPress={() => Alert.alert("notification", "this is on todo list.")}
+        >
+          <FontAwesome name="bell" size={30} color={lightTextColor} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+export default CustomHeader;
+
+const styles = StyleSheet.create({
+  container: {
+    // position: "absolute",
+    // right: 0,
+    // top: 0,
+    width: windowWidth,
+    height: 35,
+    flexDirection: "row",
+    paddingHorizontal: 10,
+  },
+  iconContainer: {
+    flex: 1,
+    justifyContent: "center",
+  },
+});
--- Screens/Home/RandomScreen.tsx
+++ Screens/Home/RandomScreen.tsx
@@ -1,8 +1,13 @@
+import { DrawerActions } from "@react-navigation/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
-import { StyleSheet } from "react-native";
+import { Alert, StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { HomeTabNavigationProps } from "../../Types/Navigations/HomeTab";
+import CustomHeader from "../../Components/HomeDrawer/CustomHeader";
+import { windowHeight, windowWidth } from "../../Constants/cssConst";
+import { HomeTabNavigationProps } from "../../types/Navigations/HomeTab";
+import { Fontisto } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 type RandomScreenProps = NativeStackScreenProps<
   HomeTabNavigationProps,
@@ -12,15 +17,93 @@ type RandomScreenProps = NativeStackScreenProps<
 const RandomScreen: React.FC<RandomScreenProps> = ({ navigation, route }) => {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={styles.safeArea}></SafeAreaView>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.background}>
+          <CustomHeader
+            toggleMenu={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+          ></CustomHeader>
+          <View style={styles.modeContainer}>
+            <View style={styles.arrow}>
+              <TouchableOpacity
+                onPress={() => {
+                  Alert.alert("", "LEFT!");
+                }}
+              >
+                <Fontisto
+                  name="caret-left"
+                  size={arrowSize}
+                  color="#FFF"
+                  style={{ borderWidth: 1 }}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.modeBtnContainer}>
+              <TouchableOpacity style={styles.modeBtn}></TouchableOpacity>
+              <TouchableOpacity style={styles.modeBtn}></TouchableOpacity>
+              <TouchableOpacity style={styles.modeBtn}></TouchableOpacity>
+            </View>
+            <View style={styles.arrow}>
+              <TouchableOpacity
+                onPress={() => {
+                  Alert.alert("", "RIGHT!");
+                }}
+              >
+                <Fontisto
+                  name="caret-right"
+                  size={arrowSize}
+                  color="#FFF"
+                  style={{ borderWidth: 1 }}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </SafeAreaView>
     </SafeAreaProvider>
   );
 };
 export default RandomScreen;
 
+const arrowSize = windowHeight * 0.08;
+
+const buttonSize = windowWidth * 0.15;
+
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
+    backgroundColor: "#FAC595",
+  },
+  background: {
+    flex: 1,
+  },
+  modeContainer: {
+    width: windowWidth,
+    height: windowHeight * 0.13,
+    marginVertical: windowHeight * 0.02,
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1,
+  },
+  arrow: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: "#900",
+  },
+  modeBtnContainer: {
+    flex: 3,
+    borderWidth: 1,
+    borderColor: "#080",
+    flexDirection: "row",
+  },
+  modeBtn: {
+    width: buttonSize,
+    height: buttonSize,
+    borderRadius: buttonSize / 2,
+    marginHorizontal: windowWidth * 0.02,
+    borderWidth: 1,
+    borderColor: "#FFF",
     backgroundColor: "#FFF",
   },
 });
