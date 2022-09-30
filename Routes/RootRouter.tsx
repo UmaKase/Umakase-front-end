7b23a7a99a988af8fd07d04aecd693f15566c251
iori0213 <iori19980213@gmail.com>
2022-10-01 03:49:58 +0800

    update interceptors and root router logic

--- Routes/RootRouter.tsx
+++ Routes/RootRouter.tsx
@@ -5,59 +5,20 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AuthNavigation from "../Navigations/AuthNavigation";
 import HomeStackNavigation from "../Navigations/HomeStackNavigation";
 //import secureStore
-import * as SecureStore from "expo-secure-store";
-import axios from "axios";
 import { AuthAPI } from "../Constants/backendAPI";
-import {
-  ACCESS_KEY,
-  CONFIG_KEY,
-  REFRESH_KEY,
-} from "../Constants/securestoreKey";
 import LoadingSpinner from "../Components/Auth/LoadingSpinner";
 import { RootNavigationProps } from "../Types/Navigations/Root";
 import customAxiosInstance from "../Utils/customAxiosInstance";
 
 const RootRouter: React.FC = () => {
   const Stack = createNativeStackNavigator<RootNavigationProps>();
-  //fetching data status
+  // fetching phase state
   const [fetching, setFetching] = useState<boolean>(true);
-  // ANCHOR start token validation to do conditional navigation
+  // access return from token validation
   const [access, setAccess] = useState<boolean>();
-  const [accessToken, setAccessToken] = useState<string>("");
-  const tokenValidation = async () => {
-    //use this while the account is not logout and you reinstall the DB
-    // await SecureStore.deleteItemAsync(ACCESS_KEY);
-    // await SecureStore.deleteItemAsync(CONFIG_KEY);
-    const localAccessToken = await SecureStore.getItemAsync(ACCESS_KEY);
-    console.log("accessToken:", localAccessToken);
-    const localRefreshToken = await SecureStore.getItemAsync(REFRESH_KEY);
-    if (!localAccessToken || !localRefreshToken) {
-      return false;
-    }
-    const accessResult = await axios({
-      method: "post",
-      url: `${AuthAPI}/token/access`,
-      headers: { Authorization: `Bearer ${localAccessToken}` },
-    });
-    if (accessResult.data.ok) {
-      setAccessToken(localAccessToken);
-      console.log("success");
-      return true;
-    }
-    const refreshResult = await axios({
-      method: "post",
-      url: `${AuthAPI}/token/refresh`,
-      headers: { Authorization: `Bearer ${REFRESH_KEY}` },
-    });
-    if (refreshResult.status != 200) {
-      console.log(refreshResult);
-      return false;
-    }
-    await SecureStore.setItemAsync(ACCESS_KEY, refreshResult.data.accessToken);
-    return true;
-  };
 
-  const authentication = async () => {
+  // token validation request
+  const tokenValidation = async () => {
     customAxiosInstance({
       method: "post",
       url: `${AuthAPI}/token/access`,
@@ -70,18 +31,21 @@ const RootRouter: React.FC = () => {
         }
       })
       .catch((e) => {
-        console.log(e.response.data);
+        console.log("token validation error:", e.response.data);
         setAccess(false);
       });
   };
 
+  // initial token validation
   useEffect(() => {
-    // SecureStore.deleteItemAsync(CONFIG_KEY);
-    authentication();
+    if (access === undefined) {
+      tokenValidation();
+    }
   }, []);
 
+  // after setAccess state end the fetching phase
   useEffect(() => {
-    if (access) {
+    if (access !== undefined) {
       setFetching(false);
     }
   }, [access]);
