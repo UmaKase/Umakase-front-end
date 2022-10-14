04121c38dd19a97bfcf026f7002c596c9233730c
iori0213 <iori19980213@gmail.com>
2022-10-15 02:31:00 +0800

    update navigation name type

--- Navigations/HomeDrawerNavigation.tsx
+++ Navigations/HomeDrawerNavigation.tsx
@@ -1,20 +1,28 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { createDrawerNavigator, DrawerContent } from "@react-navigation/drawer";
+import {
+  createDrawerNavigator,
+  DrawerContent,
+  DrawerItem,
+} from "@react-navigation/drawer";
 import RandomScreen from "../Screens/Home/RandomScreen";
 import { HomeDrawerNavigationProps } from "../Types/Navigations/HomeDrawer";
 import {
   darkTextColor,
   drawerColor,
   lightTextColor,
+  windowHeight,
+  windowWidth,
 } from "../Constants/cssConst";
 
 import { ProfileStackScreen } from "./ProfileStackNavigation";
 import RoomStackNavigation from "./DrawerNavigation/RoomStackNavigation";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const Drawer = createDrawerNavigator<HomeDrawerNavigationProps>();
 
 const HomeDrawerNavigation: React.FC = () => {
+  const logoutFunction = () => {};
   return (
     <Drawer.Navigator
       initialRouteName="RandomScreen"
@@ -31,18 +39,25 @@ const HomeDrawerNavigation: React.FC = () => {
         name="ProfileNavigation"
         component={ProfileStackScreen}
       ></Drawer.Screen>
+      <Drawer.Screen name="RandomScreen" component={RandomScreen} />
+
       <Drawer.Screen
-        name="RandomScreen"
-        component={RandomScreen}
+        name="Room"
+        component={RoomStackNavigation}
         options={{
-          drawerContentContainerStyle: {
+          drawerItemStyle: {
             borderBottomWidth: 1,
             borderBottomColor: "#FFF",
           },
         }}
       />
-
-      <Drawer.Screen name="Room" component={RoomStackNavigation} />
+      {/* <DrawerItem
+        label="logout"
+        onPress={() => logoutFunction()}
+        icon={() => (
+          <MaterialIcons name="logout" size={windowWidth * 0.05} color="#FFF" />
+        )}
+      /> */}
 
       {/* <DrawerContent navigation={}></DrawerContent> */}
     </Drawer.Navigator>
