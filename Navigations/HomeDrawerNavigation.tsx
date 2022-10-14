dc7f2a2b11e73f21253eacb7f3ecdb52c2b5d7af
iori0213 <iori19980213@gmail.com>
2022-10-15 03:28:07 +0800

    create deivde line and add logout UI

--- Navigations/HomeDrawerNavigation.tsx
+++ Navigations/HomeDrawerNavigation.tsx
@@ -1,14 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import React from "react";
 import {
   createDrawerNavigator,
-  DrawerContent,
+  DrawerContentComponentProps,
+  DrawerContentScrollView,
   DrawerItem,
+  DrawerItemList,
 } from "@react-navigation/drawer";
 import RandomScreen from "../Screens/Home/RandomScreen";
 import { HomeDrawerNavigationProps } from "../Types/Navigations/HomeDrawer";
 import {
-  darkTextColor,
   drawerColor,
   lightTextColor,
   windowHeight,
@@ -17,12 +18,46 @@ import {
 
 import { ProfileStackScreen } from "./ProfileStackNavigation";
 import RoomStackNavigation from "./DrawerNavigation/RoomStackNavigation";
-import { MaterialIcons } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
 
 const Drawer = createDrawerNavigator<HomeDrawerNavigationProps>();
 
 const HomeDrawerNavigation: React.FC = () => {
-  const logoutFunction = () => {};
+  const logoutFunction = () => {
+    console.log("Logout button!");
+  };
+  const CustomDrawerContent = (props: DrawerContentComponentProps) => {
+    return (
+      <DrawerContentScrollView {...props}>
+        <DrawerItemList {...props} />
+        {/* devide line */}
+        <DrawerItem
+          label="_______________________________"
+          onPress={() => {}}
+          style={{
+            height: 0,
+            width: windowWidth * 0.63,
+            borderWidth: 1,
+            borderColor: "#FFF",
+            marginVertical: windowHeight * 0.02,
+          }}
+        />
+        {/* Logout Button */}
+        <DrawerItem
+          label="Logout"
+          onPress={() => logoutFunction()}
+          icon={() => (
+            <Entypo name="log-out" size={windowWidth * 0.06} color="#FFF" />
+          )}
+          labelStyle={{
+            color: "#FFF",
+            fontSize: windowWidth * 0.04,
+            fontWeight: "500",
+          }}
+        />
+      </DrawerContentScrollView>
+    );
+  };
   return (
     <Drawer.Navigator
       initialRouteName="RandomScreen"
@@ -31,9 +66,11 @@ const HomeDrawerNavigation: React.FC = () => {
         drawerStyle: {
           backgroundColor: drawerColor,
         },
-        drawerActiveTintColor: darkTextColor,
+        drawerActiveTintColor: drawerColor,
         drawerInactiveTintColor: lightTextColor,
+        drawerActiveBackgroundColor: "#FFF",
       }}
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
     >
       <Drawer.Screen
         name="ProfileNavigation"
