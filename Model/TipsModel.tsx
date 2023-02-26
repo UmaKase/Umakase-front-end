6a6a45f057ebae511a9e582ade17dc877bd76ed4
chiu <javechiu@gmail.com>
2023-02-26 22:25:18 +0900

    update Tips for paging control

--- Model/TipsModel.tsx
+++ Model/TipsModel.tsx
@@ -1,19 +1,44 @@
 import { functionTipsMessage, tipCloseStr } from "../Constants/homeConst";
 import React, { useContext, useState } from "react";
-import { Alert, Modal, Pressable, StyleSheet, Text, View } from "react-native";
+import {
+  Alert,
+  Modal,
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { TipsContext } from "../Context/TipsContext";
-import { paddingSmall, windowHeight, windowWidth } from "../Constants/cssConst";
+import {
+  backgroundColor,
+  paddingSmall,
+  windowHeight,
+  windowWidth,
+} from "../Constants/cssConst";
+import { FontAwesome, Fontisto } from "@expo/vector-icons";
 
 interface ModalProps {
   modalVisible: boolean;
   setModalVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  tipMessages: string[];
 }
 export const TipsModel: React.FunctionComponent<ModalProps> = ({
   modalVisible,
   setModalVisible,
+  tipMessages,
 }) => {
-  const { currentCategory } = useContext(TipsContext);
   const [tipPage, setTipPage] = useState(0);
+  const nextPageFunc = () => {
+    if (tipMessages.length - 1 > tipPage) setTipPage(tipPage + 1);
+    console.log("tipPage:" + tipPage);
+    console.log("tipMessages.length: " + tipMessages.length);
+    console.log("current page:" + tipPage);
+  };
+  const prevPageFunc = () => {
+    if (tipPage > 0) setTipPage(tipPage - 1);
+    console.log("current page:" + tipPage);
+  };
   return (
     <Modal
       animationType="slide"
@@ -27,9 +52,50 @@ export const TipsModel: React.FunctionComponent<ModalProps> = ({
       <View style={styles.centeredView}>
         <View style={styles.modalView}>
           <View style={{ flex: 1, justifyContent: "center" }}>
-            <Text style={styles.modalText}>
-              {functionTipsMessage[currentCategory][tipPage]}
-            </Text>
+            <View style={{ flex: 1, justifyContent: "center" }}></View>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: "row",
+                justifyContent: "center",
+              }}
+            >
+              <TouchableOpacity
+                onPress={() => {
+                  prevPageFunc();
+                }}
+                style={{ width: windowWidth * 0.1 }}
+              >
+                <FontAwesome
+                  name="arrow-circle-left"
+                  size={windowWidth * 0.1}
+                  style={
+                    tipMessages.length > 1 && tipPage > 0
+                      ? {}
+                      : { display: "none" }
+                  }
+                  color="#2196F3"
+                />
+              </TouchableOpacity>
+              <Text style={styles.modalText}>{tipMessages[tipPage]}</Text>
+              <TouchableOpacity
+                onPress={() => {
+                  nextPageFunc();
+                }}
+                style={{ width: windowWidth * 0.1 }}
+              >
+                <FontAwesome
+                  name="arrow-circle-right"
+                  style={
+                    tipMessages.length > 1 && tipMessages.length - 1 > tipPage
+                      ? {}
+                      : { display: "none" }
+                  }
+                  size={windowWidth * 0.1}
+                  color="#2196F3"
+                />
+              </TouchableOpacity>
+            </View>
           </View>
           <Pressable
             style={[styles.button, styles.buttonClose]}
@@ -42,6 +108,7 @@ export const TipsModel: React.FunctionComponent<ModalProps> = ({
     </Modal>
   );
 };
+const arrowSize = windowHeight * 0.08;
 const styles = StyleSheet.create({
   centeredView: {
     flex: 1,
@@ -55,7 +122,7 @@ const styles = StyleSheet.create({
     width: windowWidth * 0.7,
     backgroundColor: "white",
     borderRadius: 20,
-    padding: 35,
+    padding: 15,
     alignItems: "center",
     shadowColor: "#000",
     shadowOffset: {
@@ -82,8 +149,14 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
   },
+  modelPic: {
+    margin: windowWidth * 0.05,
+  },
   modalText: {
-    marginBottom: 15,
+    width: windowWidth * 0.4,
+    marginLeft: windowWidth * 0.02,
+    marginRight: windowWidth * 0.02,
+    marginBottom: windowWidth * 0.07,
     textAlign: "left",
   },
 });
