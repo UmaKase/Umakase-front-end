a41977d4b86514aed6808691e155019bf093da18
Bryan <iori19980213@gmail.com>
2023-04-12 15:31:31 +0800

    add auto navigation reset after network error

--- src/Utils/normalAxios.ts
+++ src/Utils/normalAxios.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { Alert } from "react-native";
 import { setItemAsync } from "expo-secure-store";
-import { rootNavigationRef } from "../Ref";
+import { globalNavigationService, rootNavigationRef } from "../Ref";
 import { INITIAL_STAGE_FOOD, INITIAL_STAGE_TAG } from "../Constants/securestoreKey";
+import { CommonActions } from "@react-navigation/native";
 
 const normalAxios = axios.create();
 
@@ -19,13 +20,30 @@ normalAxios.interceptors.response.use(
         const data = JSON.parse(error.config.data);
         // NOTE check if tagIds & foodIds array length > 0
         if (data.tagIds.length > 0 || data.selectedFoods.length > 0) {
-          if(data.tagIds.length > 0){
-            setItemAsync(INITIAL_STAGE_TAG, JSON.stringify(data.selectedTags));
+          try {
+            if(data.tagIds.length > 0){
+              setItemAsync(INITIAL_STAGE_TAG, JSON.stringify(data.selectedTags));
+            }
+            if(data.foodIds.length > 0){
+              setItemAsync(INITIAL_STAGE_FOOD, JSON.stringify(data.selectedFoods));
+            }
+            // auto reset the navigation stack to InitialStepsNavigation to prevent user from submitting the empty info form
+            Alert.alert("ERROR", "Previous setting had been saved, information will inject automatically on next time open the app, you can close the app safely. :D", [
+              {
+                text: "OK",
+                onPress: () => {
+                  globalNavigationService("HomeStack")?.dispatch(CommonActions.reset({
+                    index: 0,
+                    routes: [
+                      { name: 'InitialStepsNavigation' },
+                    ],
+                  }))
+                }
+              }
+            ])
+          } catch (error) {
+            console.log('normalAxios: error when saving data to secure store', error);
           }
-          if(data.foodIds.length > 0){
-            setItemAsync(INITIAL_STAGE_FOOD, JSON.stringify(data.selectedFoods));
-          }
-          Alert.alert("ERROR", "Previous setting had been saved, information will inject automatically on next time open the app, you can close the app safely. :D")
         } else {
           console.log("error: foodIds length is 0!")
         }
